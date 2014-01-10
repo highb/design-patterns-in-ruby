d7ec26794e2a6ee484f39b52cfa6a4ab76991abb
Brandon High <brandon.high@puppetlabs.com>
2014-01-10 15:01:37 -0800

    Example shitty implementation of multi-format report

--- report.rb
+++ report.rb
@@ -4,17 +4,29 @@ class Report
         @text = ['Things are', 'super great']
     end
 
-    def output_report
-        puts '<html>'
-        puts '<head>'
-        puts "<title>#{@title}</title>"
-        puts '</head>'
-        puts '<body'
-        @text.each do |line|
-            puts "<p>#{line}</p>"
+    def output_report format
+        if format == :plain
+            puts "*** #{@title} ***"
+        elsif foramt == :html
+            puts '<html>'
+            puts '<head>'
+            puts "<title>#{@title}</title>"
+            puts '</head>'
+            puts '<body>'
         end
-        puts '</body>'
-        puts '</html>'
+
+        @text.each do |line|
+            if format == :plain
+                puts line
+            else
+                puts "<p>#{line}</p>"
+            end
+        end
+
+        if format == :html
+            puts '</body>'
+            puts '</html>'
+        end
     end
 end
 
